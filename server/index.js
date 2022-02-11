7e67aafccfd245a4c15adec119ae10d19151d7f5
Dylan Honeyman <dylan@dylans-iMac.local>
2022-02-11 10:34:24 -0600

    refresh token

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 const querystring = require('querystring')
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
@@ -13,14 +14,14 @@ const generateRandomString = length => {
     let text = '';
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     for (let i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
-  };
-  
-  
-  const stateKey = 'spotify_auth_state';
-  
+};
+
+
+const stateKey = 'spotify_auth_state';
+
 
 app.get('/login', (req, res) => {
     const state = generateRandomString(16);
@@ -28,19 +29,62 @@ app.get('/login', (req, res) => {
 
     const scope = 'user-read-private user-read-email';
     const queryParams = querystring.stringify({
-      client_id: CLIENT_ID,
-      response_type: 'code',
-      redirect_uri: REDIRECT_URI,
-      state: state,
-      scope: scope,
+        client_id: CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: REDIRECT_URI,
+        state: state,
+        scope: scope,
     });
-  
+
     res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
-  });
-
-
-
-const port = 8888;
-app.listen(port, () =>{
-    console.log(`express app listening at http://localhost:${port}`)
 });
+
+app.get('/callback', (req, res) => {
+    const code = req.query.code || null;
+  
+    axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: querystring.stringify({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: REDIRECT_URI
+      }),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      },
+    })
+    .then(response => {
+        if (response.status === 200) {
+    
+          const { access_token, token_type } = response.data;
+    
+          axios.get('https://api.spotify.com/v1/me', {
+            headers: {
+              Authorization: `${token_type} ${access_token}`
+            }
+          })
+            .then(response => {
+              res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+            })
+            .catch(error => {
+              res.send(error);
+            });
+    
+        } else {
+          res.send(response);
+        }
+      })
+      .catch(error => {
+        res.send(error);
+      });
+    })
+    
+
+
+
+    const port = 8888;
+    app.listen(port, () => {
+        console.log(`express app listening at http://localhost:${port}`)
+    });
